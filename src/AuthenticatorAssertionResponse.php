1c3b10492509c40333a222e840a13956e2b7dfb6
Mahdi Hasanpour <mh.hasnapour@gmail.com>
2025-10-23 16:27:59 +0330

    assertation response done

--- src/AuthenticatorAssertionResponse.php
+++ src/AuthenticatorAssertionResponse.php
@@ -3,8 +3,19 @@
 namespace Mh828\WebApisWebauthn;
 
 use Mh828\WebApisWebauthn\Abstracts\PublicKeyCredential;
+use Mh828\WebApisWebauthn\Helpers\General;
 
 class AuthenticatorAssertionResponse extends PublicKeyCredential
 {
 
+    public function verifySignature(AuthenticatorAttestationResponse $publicKeyCredential): bool
+    {
+        return openssl_verify(
+                General::base64_decode_url($this->response->authenticatorData) .
+                openssl_digest(General::base64_decode_url($this->response->clientDataJSON), 'SHA256', true),
+                General::base64_decode_url($this->response->signature),
+                $publicKeyCredential->getPublicKey(),
+                $publicKeyCredential->getOpenSSLEquivalenceAlgorithm() ?? OPENSSL_ALGO_SHA256
+            ) === 1;
+    }
 }
